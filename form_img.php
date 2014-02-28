73c045b2bf9b871d8b7ae3b72467791977321b1b
Valentin <valentin.rexand@sfr.fr>
2014-02-28 14:48:23 +0100

    modifications (WIP)

--- form_img.php
+++ form_img.php
@@ -13,22 +13,22 @@
 		<p><input type="text" id="auteur" name="auteur" value="<?php if(isset($_POST['auteur'])){ $auteur=htmlspecialchars($_POST['auteur']); echo $auteur;}else{ echo '';}?>"></p>
 		<label for="description">Description<span class="star"> *</span></label>
 		<p><textarea name="description" id="description" cols="100" rows="20"><?php if(isset($_POST['description'])){ $description=htmlspecialchars($_POST['description']);echo $description;}else{ echo '';}?></textarea></p>
-		<input type="file" name="fichier"/>
+		<input type="file" name="image"/>
 		<input type="submit" value="upload">
 	</form>
 
 <?php
-	if(isset($_POST['nom']) && (!empty($_POST['nom'])) && (isset($_POST['auteur'])) && (!empty($_POST['auteur'])) && (isset($_POST['description'])) && (!empty($_POST['edescription'])) && (isset($_FILES['fichier']))){
+	if(isset($_POST['nom']) && (!empty($_POST['nom'])) && (isset($_POST['auteur'])) && (!empty($_POST['auteur'])) && (isset($_POST['description'])) && (!empty($_POST['edescription'])) && (isset($_FILES['image']))){
 		$query="INSERT INTO galerie_php (nom, auteur, description, `date`, nom_fichier) 
-		VALUES (".$db->quote($_POST['nom']).",".$db->quote($_POST['auteur']).",".$db->quote($_POST['description']).",NOW(),".$db->quote($_FILES['fichier']).")";
-		$resultat=$db->exec($query);
+		VALUES (".$db->quote($_POST['nom']).",".$db->quote($_POST['auteur']).",".$db->quote($_POST['description']).",NOW(),".$db->quote($_FILES['image']).")";
+		//$resultat=$db->exec($query);
 		echo '<p>L\'image a été envoyée</p>';
 	} else {
 		echo '<p>Veuillez remplir les champs<span class="star"> *</span></p>';
 	}
 
-	/*if(isset($_FILES['fichier'])){
-		move_uploaded_file($_FILES['fichier']['tmp_name'], $_FILES['fichier']['image']);
+	/*if(isset($_FILES['image'])){
+		move_uploaded_file($_FILES['image']['tmp_name'], $_FILES['image']['image']);
 	}*/
 
 	echo '<p><a href="index.php">accueil</a></p>';
--- image.php
+++ image.php
@@ -1,20 +1,25 @@
 <?php
-	
 	$titre='image';
 	require_once('header.inc.php');
 	require_once('config.php');
 
 	if(isset($_GET['id'])){
-			$id=htmlspecialchars($_GET['id']);
-			$sql="SELECT `date`, auteur, nom, description, nom_fichier FROM galerie_php WHERE id=".$db->quote($id);
-			$resultat=$db->query($sql);
-			foreach ($resultat as $ligne) {
-				echo '<h2>'.$ligne['nom'].'</h2>';
-				echo '<p>'.$ligne['date'].' - '.$ligne['auteur'].'</p>';
-				echo '<img src="images/'.$ligne['nom_fichier'].'" alt="Image '.$ligne['nom'].'"/>';
-				echo '<p>'.$ligne['description'].'</p>';
-			}
+		$id=htmlspecialchars($_GET['id']);
+		$sql="SELECT * FROM galerie_php WHERE id=".$db->quote($id);
+		$resultat=$db->query($sql);
+		foreach ($resultat as $ligne) {
+			echo '<h2>'.$ligne['nom'].'</h2>';
+			echo '<p>'.$ligne['date'].' - '.$ligne['auteur'].'</p>';
+			echo '<img src="images/'.$ligne['nom_fichier'].'" alt="Image '.$ligne['nom'].'"/>';
+			echo '<p>'.$ligne['description'].'</p>';
+		}
 	}
+
+	
+	/*if(isset($_SESSION['admin'])){
+		echo'<p><a href="modif.php?id='.$id.'>Modifier</a></p>';*/
+		/* WIP requète PDO pr récup l'id?*/
+	/*}*/
 
 	echo '<p><a href="index.php">&lt;&ndash; Retour galerie</a></p>';
 
