72374779480401360f88bd67bcbddae95359ef79
Valentin <valentin.rexand@sfr.fr>
2014-03-07 14:52:34 +0100

    correction bugs de merge

--- ajax.php
+++ ajax.php
@@ -1,6 +1,7 @@
 <?php
 header ('Content-Type: application/json');
 require_once('config.php');
+require_once('connexion.php');
 
 
 $query="SELECT `date` FROM galerie_php ORDER BY `date` DESC";
--- config.php
+++ config.php
@@ -4,14 +4,8 @@
 	$config['img_par_page']=5;
 	$config['dossier']='images/';
 
-	$adminpass='adminpasswordgaleriephp';
+	$config['adminpass']='adminpasswordgaleriephp';
 
-	$sdn='mysql:dbname=3wa;host=localhost';
-	$username='3wa';
-	$password='berezina';
-	try{
-		$db= new PDO($sdn, $username, $password);
-		$db->exec('SET CHARACTER SET UTF8');//pour afficher correctement les accents et autres
-	} catch (Exception $e){
-		die('Impossible de se connecter');
-	}
+	$config['dsn']='mysql:dbname=3wa;host=localhost';
+	$config['username']='3wa';
+	$config['password']='berezina';
