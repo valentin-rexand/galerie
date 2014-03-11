3603fe3c33aa23d236599ad5961e2e52d6cba37c
Valentin <valentin.rexand@sfr.fr>
2014-03-11 17:24:49 +0100

    init modifs page connexion

--- connect.php
+++ connect.php
@@ -6,6 +6,8 @@
 
 	if(isset($_SESSION['admin'])){
 		echo '<p>Bonjour Admin<br/><a href="index.php">Accueil</a></p>';
+	} elseif(isset($_SESSION['user'])){
+		echo '<p>Bonjour <br/><a href="index.php">Accueil</a></p>';
 	} else {
 		if(isset($_POST['mdp']) && ($_POST['mdp']==$config['adminpass'])){
 			$_SESSION['admin']=true;
@@ -13,8 +15,12 @@
 		} else {
 ?>
 		<form method="post">
-			<label for="mdp">password</label>
+			<label for="login">Login</label>
+			<p><input type="text" id="login" name="login"></p>
+
+			<label for="mdp">Password</label>
 			<p><input type="password" id="mdp" name="mdp"/></p>
+
 			<p><input type="submit" value="Valider"/></p>
 		</form>
 <?php
