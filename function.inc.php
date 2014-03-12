a46d635e2a9841bb2247f6a8ba4a9e9514171a33
Valentin <valentin.rexand@sfr.fr>
2014-03-12 16:52:19 +0100

    ajout de fonction

--- function.inc.php
+++ function.inc.php
@@ -8,3 +8,13 @@
 		$ligne=$resultat->fetch();
 		return $ligne;
 	}
+
+	function nbpage(){
+		global$db;
+		global$config;
+		$sql="SELECT COUNT(id) AS count FROM galerie_php";
+		$result=$db->query($sql);
+		$taille_tableau=$result->fetchColumn();
+		$nbrpage=ceil($taille_tableau/$config['img_par_page']);
+		return $nbrpage;
+	}
